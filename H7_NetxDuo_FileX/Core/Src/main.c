51619303a25e1bb314343542c6ab80afabe08ff3
xidongxu <xi_dongxu@126.com>
2022-06-14 23:29:19 +0800

    【修改】 实现从boot向APP跳转；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- H7_NetxDuo_FileX/Core/Src/main.c
+++ H7_NetxDuo_FileX/Core/Src/main.c
@@ -51,14 +51,27 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MPU_Config(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+static void interrupt_vector_table(void)
+{
+  /* Vector Table Relocation in Internal QSPI_FLASH */
+  SCB->VTOR = QSPI_BASE;
+}
 
+static void printf_Logo(void)
+{
+  printf("       _   __       __  _  __    ___     ____   ____      \r\n");
+  printf("      / | / /___   / /_| |/ /   /   |   / __ \\ / __ \\   \r\n");
+  printf("     /  |/ // _ \\ / __/|   /   / /| |  / /_/ // /_/ /    \r\n");
+  printf("    / /|  //  __// /_ /   |   / ___ | / ____// ____/      \r\n");
+  printf("   /_/ |_/ \\___/ \\__//_/|_|  /_/  |_|/_/    /_/         \r\n");
+  printf("                                                          \r\n");
+}
 /* USER CODE END 0 */
 
 /**
@@ -68,11 +81,8 @@ static void MPU_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+  interrupt_vector_table();
   /* USER CODE END 1 */
-
-  /* MPU Configuration--------------------------------------------------------*/
-  MPU_Config();
 
   /* MCU Configuration--------------------------------------------------------*/
 
@@ -96,7 +106,8 @@ int main(void)
   MX_ETH_Init();
   MX_CRC_Init();
   /* USER CODE BEGIN 2 */
-  printf("hello world!\r\n");
+  printf_Logo();
+  printf("this is NetxDuo application!\r\n");
   /* USER CODE END 2 */
 
   MX_ThreadX_Init();
@@ -174,35 +185,6 @@ void SystemClock_Config(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
-
-/* MPU Configuration */
-
-void MPU_Config(void)
-{
-  MPU_Region_InitTypeDef MPU_InitStruct = {0};
-
-  /* Disables the MPU */
-  HAL_MPU_Disable();
-
-  /** Initializes and configures the Region and the memory to be protected
-  */
-  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-  MPU_InitStruct.BaseAddress = 0x0;
-  MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
-  MPU_InitStruct.SubRegionDisable = 0x87;
-  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
-  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
-  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
-  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-
-  HAL_MPU_ConfigRegion(&MPU_InitStruct);
-  /* Enables the MPU */
-  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
-
-}
 
 /**
   * @brief  Period elapsed callback in non blocking mode
